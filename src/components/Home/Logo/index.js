267825999015e58053dada281834005daf73a241
dokyungyang <dkyang1023@gmail.com>
2022-12-16 13:27:48 -0500

    app error

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,15 +1,26 @@
 //import { useEffect, useRef } from 'react'
 //import gsap from 'gsap-trial'
 //import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
-import LogoS from '../../../assets/images/D.png'
+import LogoS from '../../../assets/images/coop.svg'
+import LogoS2 from '../../../assets/images/dev.svg'
 import './index.scss'
 
 const Logo = () => {
   return (
     <div className="logo-container">
+      {/* <img
+        className="solid-logo"
+        src={LogoS}
+        alt="JavaScript,  Developer"
+      /> */}
       <img
         className="solid-logo"
         src={LogoS}
+        alt="JavaScript,  Developer"
+      />
+      <img
+        className="solid-logo2"
+        src={LogoS2}
         alt="JavaScript,  Developer"
       />
     </div>
